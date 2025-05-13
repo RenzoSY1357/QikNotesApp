61656696ecb08ce9db7b43d9e5cc3bf804e04580
Renzo <renzosy1357@gmail.com>
2025-05-13 22:04:06 +0200

    Revert "Merge pull request #4 from RenzoSY1357/PrimeraEtapaDesarrollo"
    This reverts commit 192304a97c1b96150a6163b5e811056f86b17a21, reversing
    changes made to 390163d6f856b1c676f54cb80daca6d1a9d3fd4b.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 // me permite definir los componentes
-
-import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+// router: 'enrutador', aun no lo usamos??
 
 import { GruposComponent } from "./vista/grupos/grupos.component";
-import { NotasComponent } from "./vista/notas/notas.component";
-import { VistaGrupoComponent } from "./vista/vista-grupo/vista-grupo.component";
-// importamos los componentes
+import { NotasComponent } from "./vista/notas/notas.component"; // <-- Add this import
+//importamos los componentes
 
 @Component({
   selector: 'app-root',
-  imports: [GruposComponent, NotasComponent, VistaGrupoComponent, CommonModule],
+  imports: [RouterOutlet, GruposComponent, NotasComponent],
   // esto registra los componentes importados, para que se puedan usar dentro de este componente
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -18,19 +17,8 @@ import { VistaGrupoComponent } from "./vista/vista-grupo/vista-grupo.component";
 
 export class AppComponent {
 
-  grupoSeleccionadoId: number | null = null;
-  grupoSeleccionadoNombre: string | null = null;
-
-  // Se guardan los datos del grupo seleccionado, recibidos con [grupoSeleccionadId] y [grupoNombre]
-  GrupoSeleccionado(grupo: { id: number, name: string }){
-    this.grupoSeleccionadoId = grupo.id;
-    this.grupoSeleccionadoNombre = grupo.name;
-  }
-
-  // Oculta los elementos de la vista grupo
-  volverAVistaPrincipal(){
-    this.grupoSeleccionadoId = null;
-    this.grupoSeleccionadoNombre = null;
-  }
+  data = {
+    title: 'QikNotesApp',
+  };
 
 }
--- src/app/vista/grupos/grupos.component.ts
+++ src/app/vista/grupos/grupos.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { NotasServicio } from '../../servicio/servicio';
 import { Grupo } from '../../modelo/modelo';
@@ -6,6 +6,7 @@ import { Grupo } from '../../modelo/modelo';
 import { CommonModule } from '@angular/common';
 // CommonModulo, lo que hace es proveer directivas de angular como *ngFor
 // ngFor se usa para desplegar listas repitiendo elementos
+
 
 @Component({
   selector: 'app-grupos',
@@ -20,15 +21,11 @@ export class GruposComponent {
   // Un array de grupos, que se pasan a traves de la interfaz, para validar los tipos
   grupos: Grupo[] = [];
 
- // Con EventEmitter, se declara el evento, para ser escuchado con "[]"
-  @Output () grupoSeleccionado = new EventEmitter<{ id: number, name:string }>();
-
-  // Injectamos las notasServicio al Componente, en este caso al array de grupos, cada vez que el servicio se actualiza, el array tambien
   constructor(private notasServicio: NotasServicio){
-    this.notasServicio.grupos$.subscribe(grupos$ => this.grupos = grupos$);
+    this.notasServicio.grupos$.subscribe(grupos => this.grupos = grupos);
   }
 
-  // Generamos un prompt para crear un grupo, llamando a la funcion del servicio addGrupo
+  // Crea un grupo
   crearGrupo(){
 
     const nombre = prompt('Nombre del grupo');
@@ -38,13 +35,8 @@ export class GruposComponent {
     }
   }
 
-  // Eliminamos un grupo pasando su id y llamando a la funcion del servicio deleteGroup
+  // Elimina un grupo
   eliminarGrupo (id: number){
     this.notasServicio.deleteGroup(id);
-  }
-
-  //Se emiten las propiedades del grupo
-  seleccionarGrupo(grupo: Grupo){
-    this.grupoSeleccionado.emit({id: grupo.id, name: grupo.name});
   }
 }
